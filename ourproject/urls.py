dc8c232f1ff284b63643175ea953bcefe776f359
Hanen <hneinsalook.123@gmail.com>
2023-01-01 14:21:42 +0200

    Merge remote-tracking branch 'origin/main' into de3

--- ourproject/urls.py
+++ ourproject/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home,name="home"),
     path('products/', views.products, name='poducts'),
     path('customer/', views.customer,name='customers'),
+    path('workers/', views.workers, name='workers'),
+
     path('login/', views.logincustomer,name='login'),
     path('logoutcustomer/', views.logoutcustomer, name='logout'),
 
@@ -14,6 +16,7 @@ urlpatterns = [
     path('homepage/',views.homepage,name='homepage'),
     path('homepageadmin/', views.homepage_admin, name='homepage_admin'),
     path('homepageworker/', views.homepage_worker, name='homepage_worker'),
+    path('customers/',views.customer,name='customer_list'),
 
     path('singup/', views.singup,name="sigup"),
 
