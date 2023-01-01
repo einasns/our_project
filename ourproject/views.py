00a663fe55f18e4e42a6729b014031ea82acb613
ssoma <somayab@ac.sce.ac.il>
2023-01-01 11:29:45 +0200

    Merge pull request #1 from einasns/customer-singup-login
    add

--- ourproject/views.py
+++ ourproject/views.py
@@ -3,88 +3,103 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 from .models import *
 from .forms import CreatUserForm
-from .decorators import unauthenticated_user,allwed_users,admin_only,unauthenticated_user_login
+from .decorators import unauthenticated_user, allwed_users, admin_only, unauthenticated_user_login
+
+
 # Create your views here.
 @unauthenticated_user
 def singup(request):
-	form = CreatUserForm()
-	if request.method == 'POST':
-		form = CreatUserForm(request.POST)
-		if form.is_valid():
-			user=form.save()
-			username=form.cleaned_data.get('username')
-			group=Group.objects.get(name='Customer')
-			user.groups.add(group)
-			messages.success(request,'Account was created for ' + username)
-			return redirect('login')
-	context = {'form':form}
-	return render(request, 'ourproject/singup.html', context)
+    form = CreatUserForm()
+    if request.method == 'POST':
+        form = CreatUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='Customer')
+            user.groups.add(group)
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'ourproject/singup.html', context)
+
 
 def logincustomer(request):
-	users_in_group = Group.objects.get(name='Customer').user_set.all()
-	if request.method=='POST':
-		username=request.POST.get('Username')
-		password=request.POST.get('Password')
-		user=authenticate(request,username=username,password=password)
-		if user is not None:
-			if user in users_in_group:
-				login(request,user)
-				return redirect('homepage')
-			else:
-				messages.info(request, 'Username OR Password incorrert')
-		else:
-			messages.info(request,'username OR password incorrert')
-	context={}
-	return render(request, 'ourproject/login_customer.html',context)
+    users_in_group = Group.objects.get(name='Customer').user_set.all()
+    if request.method == 'POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user in users_in_group:
+                login(request, user)
+                return redirect('homepage')
+            else:
+                messages.info(request, 'Username OR Password incorrert')
+        else:
+            messages.info(request, 'username OR password incorrert')
+    context = {}
+    return render(request, 'ourproject/login_customer.html', context)
+
+
 def logoutcustomer(request):
-	logout(request)
-	return redirect('login')
+    logout(request)
+    return redirect('login')
+
+
 def logoutadmin(request):
-	logout(request)
-	return redirect('loginAdmin')
+    logout(request)
+    return redirect('loginAdmin')
+
+
 def logoutworker(request):
-	logout(request)
-	return redirect('logoutworker')
+    logout(request)
+    return redirect('logoutworker')
+
+
 @unauthenticated_user
 def loginAdmin(request):
-	users_in_group = Group.objects.get(name='Admin').user_set.all()
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			if user in users_in_group:
-				login(request, user)
-				return redirect('customers')
-			else:
-				messages.info(request, 'username OR password incorrert')
-		else:
-			messages.info(request, 'username OR password incorrert')
-	context = {}
-	return render(request, 'ourproject/log_in_admin.html', context)
+    users_in_group = Group.objects.get(name='Admin').user_set.all()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user in users_in_group:
+                login(request, user)
+                return redirect('customers')
+            else:
+                messages.info(request, 'username OR password incorrert')
+        else:
+            messages.info(request, 'username OR password incorrert')
+    context = {}
+    return render(request, 'ourproject/log_in_admin.html', context)
+
+
 def loginWorker(request):
-	context={}
-	return render(request, 'ourproject/log_in_worker.html')
+    context = {}
+    return render(request, 'ourproject/log_in_worker.html')
+
+
 def home(request):
-	return render(request, 'ourproject/dashboard.html')
+    return render(request, 'ourproject/dashboard.html')
+
 
 @login_required(login_url='login')
 @admin_only
 def homepage(request):
-	return render(request, 'ourproject/homepage.html')
+    return render(request, 'ourproject/homepage.html')
 
 
 def products(request):
+    products = Product.objects.all()
 
-	products=Product.objects.all()
+    return render(request, 'ourproject/product.html', {'products': products})
 
-	return render(request, 'ourproject/product.html',{'products':products})
 
 def customer(request):
-	return render(request, 'ourproject/customer.html')
-
+    return render(request, 'ourproject/customer.html')
