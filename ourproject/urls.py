f293ebdd571d0867eab0c8c6a2d373d8fd01ce29
ssoma <somayab@ac.sce.ac.il>
2023-01-05 22:36:06 +0200

    Merge pull request #1 from einasns/customer-singup-login
    add

--- ourproject/urls.py
+++ ourproject/urls.py
@@ -7,18 +7,15 @@ urlpatterns = [
     path('products/', views.products, name='poducts'),
     # path('customer/', views.customers,name='customers'),
     path('workers/', views.workers, name='workers'),
-
     path('login/', views.logincustomer,name='login'),
     path('logoutcustomer/', views.logoutcustomer, name='logout'),
-
     path('loginAdmin/', views.loginAdmin,name='loginadmin'),
     path('loginWorker/', views.loginWorker,name='loginWorker'),
     path('homepage/',views.homepage,name='homepage'),
     path('homepageadmin/', views.homepage_admin, name='homepage_admin'),
     path('homepageworker/', views.homepage_worker, name='homepage_worker'),
     path('customers/',views.customer,name='customer_list'),
-
-
+    #path('delete_product/<bar_code>, views.delete_product '),
     path('singup/', views.singup,name="sigup"),
 
 ]
