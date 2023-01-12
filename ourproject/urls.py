1be7ed4777f5d2f9146e28759229733c32cdd887
Hanen <hneinsalook.123@gmail.com>
2023-01-12 13:39:48 +0200

    worker view feedback

--- ourproject/urls.py
+++ ourproject/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('conactus/', views.conactus, name="conactus"),
     path('review_my_order/<str:pk>/', views.review_my_order, name='review_my_order'),
     path('best_sales/', views.best_sales, name="best_sales"),
-    path('worker_view_feedback/',views.worker_view_feedback,name="worker_view_feedback")
+    path('worker_view_feedback/',views.worker_view_feedback,name="worker_view_feedback"),
     path('reviewfeedbackcustomer/', views.reviewfeedback, name="reviewfeedbackcustomer"),
 
 ]
